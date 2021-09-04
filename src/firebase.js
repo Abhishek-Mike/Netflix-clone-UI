c443ca98ab88f4c27c5e8a00e407ca3c3dd8f8a3
Abhishek Sadhu <abhisadhu.007@gmail.com>
2021-09-04 22:59:13 +0530

    netflix-clone-banner-loading

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCrdU61DkQY_zk3CqabWn6R-wGKWNrRN3Q",
+    authDomain: "netflix-clone-70f59.firebaseapp.com",
+    projectId: "netflix-clone-70f59",
+    storageBucket: "netflix-clone-70f59.appspot.com",
+    messagingSenderId: "445893796532",
+    appId: "1:445893796532:web:aa6f0e44f7c5834dee2de1",
+    measurementId: "G-K2KCWTRS4R"
+  };
