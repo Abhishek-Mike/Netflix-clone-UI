e9940a68a4bccd44613078dc5c62715c1c8ce688
Abhishek Sadhu <abhisadhu.007@gmail.com>
2021-09-06 11:18:38 +0530

    firestore-db-config

--- src/Row.js
+++ src/Row.js
@@ -5,7 +5,7 @@ import "./Row.css";
 function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
 
-  const base_url = "https://image.tmdb.org/t/p/original/";
+  const base_url = "https://image.tmdb.org/t/p/original";
   //const base_url = "https://image.tmdb.org/t/p/w500/" ;
 
   useEffect(() => {
@@ -19,22 +19,27 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
   }, [fetchUrl]);
 
   console.log(movies);
+  //Line 30, 31 for fixing dead link
 
   return (
     <div className="row">
       <h2>{title}</h2>
 
       <div className="row__posters">
-        {movies.map((movie) => (
-          <img
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            key={movie.id}
-            src={`${base_url}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
-          />
-        ))}
+        {movies.map(
+          (movie) =>
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                key={movie.id}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            )
+        )}
       </div>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyCrdU61DkQY_zk3CqabWn6R-wGKWNrRN3Q",
     authDomain: "netflix-clone-70f59.firebaseapp.com",
@@ -8,3 +10,11 @@ const firebaseConfig = {
     appId: "1:445893796532:web:aa6f0e44f7c5834dee2de1",
     measurementId: "G-K2KCWTRS4R"
   };
+
+  //initialize firebase app with firebase config, pass config object store in firebase APP
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();   //access firestore db instance
+const auth = firebase.auth();
+
+export { auth };
+export default db;
