7f93d06deed4c6f556d52b9a8c0424097b770bcb
Abhishek Sadhu <abhisadhu.007@gmail.com>
2021-09-09 11:36:01 +0530

    added-trailers-youtubeURL-path

--- src/Banner.js
+++ src/Banner.js
@@ -2,10 +2,13 @@ import axios from "./axios";
 import React, { useEffect, useState } from "react";
 import "./Banner.css";
 import requests from "./Requests";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 function Banner() {
   //search movie
   const [movie, setMovie] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState(""); // this state will capture the url , when we click the thumbnail of the poster
 
   useEffect(() => {
     async function fetchData() {
@@ -22,6 +25,32 @@ function Banner() {
   }, []);
 
   console.log(movie);
+  
+  const opts = {
+    height: "300",
+    width: "100%",
+    playerVars: {
+      autoplay: 0,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl(''); //if trailer is already open, it will hide/close video if u click it
+    } else {
+      movieTrailer(movie?.title || "")
+        .then((url) => {
+            //https://www.youtube.com/watch?v=XtMThy8QKqU
+                                //v=XtMThy8QKqU
+          const urlParams = new URLSearchParams(new URL(url).search);
+          //urlParams.get('v'); in this example url above
+          setTrailerUrl(urlParams.get('v'));
+        })
+        .catch((error) => {
+          alert('Temporarily unavailable');
+      });
+    }
+  };
 
   // truncate movie description greater tha 150 words and trim ab
   function truncate(string, n) {
@@ -42,7 +71,7 @@ function Banner() {
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
         <div className="banner_buttons">
-          <button className="banner__button">Play</button>
+          <button onClick={() => handleClick(movie)} className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
@@ -50,6 +79,7 @@ function Banner() {
         </h1>
       </div>
       <div className="banner__fadeBottom" />
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </header>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,12 @@
 import axios from "./axios";
 import React, { useEffect, useState } from "react";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState(""); // this state will capture the url , when we click the thumbnail of the poster
 
   const base_url = "https://image.tmdb.org/t/p/original";
   //const base_url = "https://image.tmdb.org/t/p/w500/" ;
@@ -21,6 +24,32 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
   console.log(movies);
   //Line 30, 31 for fixing dead link
 
+  const opts = {
+    height: "300",
+    width: "100%",
+    playerVars: {
+      autoplay: 0,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl(" "); //if trailer is already open, it will hide/close video if u click it
+    } else {
+      movieTrailer(movie?.title || "")
+        .then((url) => {
+            //https://www.youtube.com/watch?v=XtMThy8QKqU
+                                //v=XtMThy8QKqU
+          const urlParams = new URLSearchParams(new URL(url).search);
+          //urlParams.get('v'); in this example url above
+          setTrailerUrl(urlParams.get('v'));
+        })
+        .catch((error) => {
+          alert('Temporarily unavailable');
+      });
+    }
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -31,6 +60,7 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
             ((isLargeRow && movie.poster_path) ||
               (!isLargeRow && movie.backdrop_path)) && (
               <img
+                onClick={() => handleClick(movie)}
                 className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                 key={movie.id}
                 src={`${base_url}${
@@ -41,6 +71,7 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
             )
         )}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
